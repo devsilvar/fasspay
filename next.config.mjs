6143d6fd619d46420c000db3b226d1ad9b6ab26e
candie <adekanmi.adewumi@straitpay.com>
2024-09-19 12:22:18 +0100

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,11 @@
 import path from "path";
 import { fileURLToPath } from "url";
 
-// Use import.meta.url and fileURLToPath to get the __dirname equivalent in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // Enable experimental features like the new app directory (optional)
-  experimental: {
-    appDir: true,
-  },
 
   // Set up webpack aliases (e.g., alias '@' for 'src' folder)
   webpack: (config) => {
